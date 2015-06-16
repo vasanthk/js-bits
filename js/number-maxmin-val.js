9553db4834f4b0950a33da12c89fc8fd1ff53f32
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-06-16 13:00:37 -0700

    Number.MIN_VALUE < 0 // false.. WTF

---
+++ js/number-maxmin-val.js
@@ -0,0 +1,13 @@
+/**
+ * Native JS
+ *
+ * Number.MAX_VALUE and Number.MIN_VALUE
+ *
+ * Number.MAX_VALUE: 1.7976931348623157e+308
+ * Number.MIN_VALUE: 5e-324
+ */
+
+(function () {
+  console.log(Number.MAX_VALUE > 0); // true
+  console.log(Number.MAX_VALUE > 0); // false... WTF
+})();

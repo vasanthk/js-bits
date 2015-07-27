aa1dd0d7fb1f46c609eeaf9830c290173f2b48e6
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-07-26 23:22:12 -0700

    reduce() example

--- js/array-filter-map-reduce.js
+++ js/array-filter-map-reduce.js
@@ -109,3 +109,19 @@
  *  initialValue: (Optional) object used as first argument in the first (leftmost) callback.
  *
  */
+(function () {
+  var rockets = [
+    {country: 'Russia', launches: 32},
+    {country: 'US', launches: 23},
+    {country: 'China', launches: 16},
+    {country: 'Europe(ESA)', launches: 7},
+    {country: 'India', launches: 4},
+    {country: 'Japan', launches: 3}
+  ];
+
+  var sum = rockets.reduce(function (prevVal, elem) {
+    return prevVal + elem.launches;
+  }, 0);
+
+  console.log(sum);
+})();

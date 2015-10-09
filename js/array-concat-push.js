309d437341d8cabbe6a2c219448687039d7a79b9
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-10-08 22:59:07 -0700

    Array concat() vs push() - Updated example

--- js/array-concat-push.js
+++ js/array-concat-push.js
@@ -4,16 +4,24 @@
  * Reference: http://gunnariauvinen.com/difference-between-concat-and-push-in-javascript/
  */
 
-(function() {
-  // Array.concat()
-  // New array is created on concat
-  // For a couple of small arrays, this is fine. But for large arrays, or repeating this process regularly a lot of times,
-  // or working in memory-limited environments, it leaves a lot to be desired.
-  var a = [1, 2],
-      b = ["x", "y"],
-      c = [true, false];
+(function () {
+  // Array.push() modifies the original array it's pushed to
+  // The return value of the push operation is the length of the returned array.
+  var testArr = [1, 2, 3];
+  var res = testArr.push(4, 5, 6);
 
-  var d = a.concat(b, c);
-  console.log(d); // [1, 2, "x", "y", true, false];
+  console.log(res);  // 6
+  console.log(testArr); // [1, 2, 3, 4, 5, 6]
 
+  // Array.concat() returns a new array as the result. The original array is unchanged.
+  // Gotcha: In the case of objects, instead of copying objects into the new array, the references are copied instead.
+  var test = [1, 2, 3]; // [1, 2, 3]
+  var example = [{test: 'test value'}, 'a', 'b', 4, 5];
+  var concatExample = test.concat(example); // [1, 2, 3, { test: 'test value'}, 'a', 'b', 4, 5]
+
+  // Modifying values
+  example[0].test = 'a changed value';
+  console.log(concatExample[3].test); // Object { test: "a changed value"}
+  example[1] = 'dog';
+  console.log(concatExample[4]); // 'a'
 })();

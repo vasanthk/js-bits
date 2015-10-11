40096a9dd2e3b1aa6513417454717f48fc2dbcbd
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-10-11 15:58:36 -0700

    More coercion examples

--- js/coercion.js
+++ js/coercion.js
@@ -40,4 +40,30 @@
   console.log(null == undefined);   // true
 
   console.log(' \t\r\n ' == 0);     // true
+
+  // Array
+  var a = [1, 2, 3];
+  var b = [1, 2, 3];
+
+  console.log(a == b);            // false
+  console.log(a === b);           // false
+
+  // Object
+  var c = {x: 1, y: 2};
+  var d = {x: 1, y: 2};
+
+  console.log(c == d);            // false
+  console.log(c === d);           // false
+
+  // String
+  var e = "text";
+  var f = "te" + "xt";
+
+  console.log(e == f);            // true
+  console.log(e === f);           // true
+
+  // Here the == operator is checking the values of the two objects and returning true,
+  // but the === is seeing that they're not the same type and returning false.
+  console.log("abc" == new String("abc"));    // true
+  console.log("abc" === new String("abc"));   // false
 })();

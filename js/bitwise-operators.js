f8fe12e84f2ea37299e2eb4fc7ce5afb315e93ee
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-30 01:43:44 -0800

    Various bitwise operators

--- js/bitwise-operators.js
+++ js/bitwise-operators.js
@@ -2,9 +2,44 @@
  * Bitwise operators in JavaScript
  *
  * @Reference:
+ * http://michalbe.blogspot.com/2013/03/javascript-less-known-parts-bitwise.html
  * http://www.w3schools.com/jsref/jsref_operators.asp
  * http://stackoverflow.com/questions/654057/where-would-i-use-a-bitwise-operator-in-javascript
  */
+
+// Various bitwise operators
+var a = 5;
+var b = 13;
+
+// a | b - OR
+// if in any of the given numbers corresponding bit is '1', then the result is '1'
+console.log('or', a|b); // 13
+
+// a & b - AND
+// if in both of the given numbers corresponding bit is '1', then the result is '1'
+console.log('and', a&b); // 5
+
+// a ^ b - XOR
+// if in one of the given numbers (not both) corresponding bit is '1', then the result is '1'
+console.log('xor', a^b); // 8
+
+// ~a - NOT
+// inverts all the bits
+console.log('not', ~a); // -6
+
+// a >> b - RIGHT SHIFT
+// shift binary representation of 'a' for 'b' bits to the right, discarding bits shifted off
+console.log('rs', a>>b); // 0
+
+// a << b - LEFT SHIFT
+// shift binary representation of 'a' for 'b' bits to the right, shifting in zeros from the right
+console.log('ls', a<<b); // 40960
+
+// a >>> b - ZERO FILLED RIGHT SHIFT
+// shift binary representation of 'a' for 'b' bits to the right, discarding bits shifted off,
+// and shifting in zeros from the left.
+console.log('zfrs', a>>>b); // 0
+
 
 // Practical Use case of Bitwise operators in JavaScript
 var hex = 'ffaadd';

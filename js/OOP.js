f7fc180d0039f3b061e2e5d7cafd9342d6218a8d
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-12-07 01:31:43 -0800

    Object.create()

--- js/OOP.js
+++ js/OOP.js
@@ -21,8 +21,11 @@
  * http://javascriptissexy.com/oop-in-javascript-what-you-need-to-know/
  */
 
-// Combination Constructor/Prototype Pattern
+/**
+ * ENCAPSULATION
+ */
 
+// Combination Constructor/Prototype Pattern
 function User (theName, theEmail) {
   this.name = theName;
   this.email = theEmail;
@@ -60,3 +63,27 @@ firstUser.showNameAndScores(); //Richard Scores: 15,10​
 secondUser = new User("Peter", "Peter@examnple.com");
 secondUser.saveScore(18);
 secondUser.showNameAndScores(); //Peter Scores: 18
+
+/**
+ * Object.create()
+ *
+ * The crux of the matter with this Object.create method is that you pass into it an object that you want to inherit from,
+ * and it returns a new object that inherits from the object you passed into it.
+ */
+Object.create = function (o) {
+  //It creates a temporary constructor F()​
+  function F() {
+  }
+  //And set the prototype of the this constructor to the parametric (passed-in) o object​
+  //so that the F() constructor now inherits all the properties and methods of o​
+  F.prototype = o;
+
+  //Then it returns a new, empty object (an instance of F())​
+  //Note that this instance of F inherits from the passed-in (parametric object) o object. ​
+  //Or you can say it copied all of the o object's properties and methods​
+  return new F();
+};
+
+// Parasitic Combination Inheritance Pattern
+
+

ea3a4b4e596acdc80db195774cb5b71010175ce6
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-01-23 17:13:46 -0800

    Object.create() simple polyfill

--- js/object-create.js
+++ js/object-create.js
@@ -89,3 +89,27 @@
     }
   });
 })();
+
+/**
+ * Simple Object.create() polyfill
+ */
+/**
+ * Object.create()
+ *
+ * The crux of the matter with this Object.create method is that you pass into it an object that you want to inherit from,
+ * and it returns a new object that inherits from the object you passed into it.
+ */
+Object.create = function (o) {
+  //It creates a temporary constructor F()​
+  function F() {
+  }
+
+  //And set the prototype of the this constructor to the parametric (passed-in) o object​
+  //so that the F() constructor now inherits all the properties and methods of o​
+  F.prototype = o;
+
+  //Then it returns a new, empty object (an instance of F())​
+  //Note that this instance of F inherits from the passed-in (parametric object) o object. ​
+  //Or you can say it copied all of the o object's properties and methods​
+  return new F();
+};

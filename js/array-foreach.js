038be3ac375d9c52d777e1de7d0fccbfd5a8a7d9
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-03 00:03:09 -0800

    Array forEach example

--- js/array-foreach.js
+++ js/array-foreach.js
@@ -4,6 +4,8 @@
  * @Reference:
  * http://stackoverflow.com/questions/23614054/javascript-nuances-of-myarray-foreach-vs-for-loop
  * http://javascriptplayground.com/blog/2012/06/writing-javascript-polyfill/
+ * http://www.2ality.com/2011/04/iterating-over-arrays-and-objects-in.html
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
  *
  */
 
@@ -17,3 +19,15 @@ Array.prototype.forEach = function (callback, thisArg) {
     callback.call(thisArg, this[i], i, this);
   }
 };
+
+// Example of forEach()
+function logArrayElements(currElement, currIndex, originalArray) {
+  console.log('a[' + currIndex + '] = ' + currElement);
+}
+
+// Note there is no member at 2 so it isn't visited
+[2, 5, , 9].forEach(logArrayElements);
+// logs:
+// a[0] = 2
+// a[1] = 5
+// a[3] = 9

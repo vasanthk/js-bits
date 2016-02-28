e5d5a857906014c18bf57b2cee3c5b0baf01d6dd
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-28 09:47:11 -0800

    To convert integers to binary

--- js/bitwise-operators.js
+++ js/bitwise-operators.js
@@ -13,15 +13,15 @@ var b = 13;
 
 // a | b - OR
 // if in any of the given numbers corresponding bit is '1', then the result is '1'
-console.log('or', a|b); // 13
+console.log('or', a | b); // 13
 
 // a & b - AND
 // if in both of the given numbers corresponding bit is '1', then the result is '1'
-console.log('and', a&b); // 5
+console.log('and', a & b); // 5
 
 // a ^ b - XOR
 // if in one of the given numbers (not both) corresponding bit is '1', then the result is '1'
-console.log('xor', a^b); // 8
+console.log('xor', a ^ b); // 8
 
 // ~a - NOT
 // inverts all the bits
@@ -29,16 +29,16 @@ console.log('not', ~a); // -6
 
 // a >> b - RIGHT SHIFT
 // shift binary representation of 'a' for 'b' bits to the right, discarding bits shifted off
-console.log('rs', a>>b); // 0
+console.log('rs', a >> b); // 0
 
 // a << b - LEFT SHIFT
 // shift binary representation of 'a' for 'b' bits to the right, shifting in zeros from the right
-console.log('ls', a<<b); // 40960
+console.log('ls', a << b); // 40960
 
 // a >>> b - ZERO FILLED RIGHT SHIFT
 // shift binary representation of 'a' for 'b' bits to the right, discarding bits shifted off,
 // and shifting in zeros from the left.
-console.log('zfrs', a>>>b); // 0
+console.log('zfrs', a >>> b); // 0
 
 
 // Practical Use case of Bitwise operators in JavaScript
@@ -47,9 +47,9 @@ var rgb = parseInt(hex, 16);    // value is 1675421
 
 // & 0xFF ensures that if bytes are longer than 8 bits, the rest of the bits are cleared.
 // http://stackoverflow.com/a/14713134/1672655
-var red   = (rgb >> 16) & 0xFF; // returns 255
+var red = (rgb >> 16) & 0xFF; // returns 255
 var green = (rgb >> 8) & 0xFF;  // 170
-var blue  = rgb & 0xFF;         // 221
+var blue = rgb & 0xFF;         // 221
 
 // In JavaScript, you can use a double bitwise negation (~~n) as a replacement for Math.floor(n)
 // (if n is a positive number) or parseInt(n, 10) (even if n is negative). n|n and n&n always yield the same results as ~~n.
@@ -58,14 +58,18 @@ n; // 3.141592653589793
 Math.floor(n); // 3
 parseInt(n, 10); // 3
 ~~n; // 3
-n|n; // 3
-n&n; // 3
+n | n; // 3
+n & n; // 3
 
 // ~~n works as a replacement for parseInt() with negative numbers…
 ~~(-n); // -3
-(-n)|(-n); // -3
-(-n)&(-n); // -3
+(-n) | (-n); // -3
+(-n) & (-n); // -3
 parseInt(-n, 10); // -3
 // …although it doesn’t replace Math.floor() for negative numbers
 Math.floor(-n); // -4
 
+// To convert integers to binary
+// To check dec number's binary value, we use .toString() method with base argument - '2' for binary
+var number = 5;
+console.log(number.toString(2));  // 101

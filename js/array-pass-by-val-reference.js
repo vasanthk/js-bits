2ee312685e344b6667211f7f07f13e236fb9b492
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-03-20 03:58:19 -0700

    Clarify misconception about pass by value
     closes #14

--- js/array-pass-by-val-reference.js
+++ js/array-pass-by-val-reference.js
@@ -18,7 +18,13 @@ alert(a);	// will output the new a - [3, 'my new post', {345}, 4]
 
 // Pass by value
 // This can be done by using the native array method – “slice()”
-// The ‘slice’ method in this case will return a shallow copy of the array.
+//
+// Basically, the slice() operation clones the array and returns the reference to the new array. Also note that:
+// - For object references (and not the actual object), slice copies object references into the new array.
+//   Both the original and new array refer to the same object. If a referenced object changes, the changes are visible to both the new and original arrays.
+// - For strings and numbers, slice copies strings and numbers into the new array.
+//   Changes to the string or number in one array does not affect the other array.
+//
 var a = [3, 'my new post', {345}];
 
 function renderData(a) {
@@ -27,3 +33,8 @@ function renderData(a) {
 
 renderData(a.slice());
 alert(a);	// will output the original a - [3, 'my new post', {345}]
+
+// If you did want to copy array's object references by value -- Use JSON.parse(JSON.stringify(array))
+// Note: It does have a few caveats with copying functions/dates as values.
+// For more info check: https://github.com/vasanthk/js-bits/blob/master/js/object-clone.js
+var tempArray = JSON.parse(JSON.stringify(mainArray));
